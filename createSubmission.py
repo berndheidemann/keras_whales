d040c2536dc0019f08fcf0e843de2fc9c0ee30f5
bernd.heidemann@googlemail.com <bernd.heidemann@googlemail.com>
2019-02-19 17:45:52 +0100

    added data augmentation

--- createSubmission.py
+++ createSubmission.py
@@ -13,16 +13,43 @@ from keras.preprocessing import image
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
-filenames = os.listdir("./data/test")[0:100]
+filenames = os.listdir("./data/test")
 nb_samples = len(filenames)
+
+calculate_mean_std_on_train=True
 
 trainPath="./data/train.csv"
 df=pd.read_csv(trainPath, sep=",").astype(dtype=str)
+
+if calculate_mean_std_on_train:
+    print("Preparing images for statistics")
+    X_train = np.zeros((df.shape[0], 100, 100, 3))
+    count = 0
+
+    for fig in df['Image']:
+        # load images into images of size 100x100x3
+        img = image.load_img("./data/train/" + fig, target_size=(100, 100, 3))
+        x = image.img_to_array(img)
+        x = preprocess_input(x)
+
+        X_train[count] = x
+        if (count % 500 == 0):
+            print("Processing image: ", count + 1, ", ", fig)
+        count += 1
+    X_train /= 255
+
+    generator = ImageDataGenerator(featurewise_center=True,
+                                   featurewise_std_normalization=True)
+
+# Calculate statistics on train dataset
+    generator.fit(X_train)
+
 
 
 values = np.array(df['Id'])
 label_encoder = LabelEncoder()
 integer_encoded = label_encoder.fit_transform(values)
+
 
 
 X_test = np.zeros((nb_samples, 100, 100, 3))
@@ -38,6 +65,28 @@ for fig in filenames:
     if (count % 500 == 0):
         print("Processing image: ", count + 1, ", ", fig)
     count += 1
+
+X_test/=255
+
+
+if calculate_mean_std_on_train:
+    # Apply featurewise_center to test-data with statistics from train data
+    X_test -= generator.mean
+    # Apply featurewise_std_normalization to test-data with statistics from train data
+    X_test /= (generator.std + K.epsilon())
+else:
+    # Apply featurewise_center to test-data with statistics from train data
+    X_test -= [[[0.06615361, 0.06615361, 0.04348451]]]
+    # Apply featurewise_std_normalization to test-data with statistics from train data
+    X_test /= [[[0.14075036, 0.15493952, 0.15001519]]]
+
+'''
+generator.mean  ---> [[[0.06615361 0.06615361 0.04348451]]]
+(generator.std + K.epsilon()) ---> [[[0.14075036 0.15493952 0.15001519]]]
+'''
+
+print("generator.mean" + str(generator.mean))
+print("(generator.std + K.epsilon())" + str((generator.std + K.epsilon())))
 
 def plotImgs(x, y):
     ax = []
@@ -58,11 +107,11 @@ def plotImgs(x, y):
         plt.imshow(image.astype('uint8'))
     plt.show()
 
-plotImgs(X_test, np.zeros((100)))
 
-model = load_model("./saves/model-99-0.8968.h5")
+model = load_model("./saves/model-955-0.5748.h5")
 preds = model.predict(X_test)
 submission=[]
+plotImgs(X_test, np.zeros((100)))
 
 for i in range(nb_samples):
     print(preds[i].argsort()[-5:])
--- kernel_v2.py
+++ kernel_v2.py
@@ -65,16 +65,16 @@ y.shape
 
 
 datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
+        featurewise_center=True,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        featurewise_std_normalization=True,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
         zca_whitening=False,  # apply ZCA whitening
-        rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.3, # Randomly zoom image
-        shear_range=0.3, #Scherwinkel
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image
+        shear_range=0.1, #Scherwinkel
+        width_shift_range=0.05,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.05,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=False,  # randomly flip images
         vertical_flip=False)  # randomly flip images
 
